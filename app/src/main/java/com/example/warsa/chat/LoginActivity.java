280912557f0c425a4a90e6efb8141ab1301eff40
Warsame Eg <warsameegal0@gmail.com>
2017-06-23 16:22:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/warsa/chat/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.warsa.chat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void redirect(View view) {
+        final Intent intent = new Intent(LoginActivity.this, RegisterActivity.class); //connect this Activity to registerActivity
+
+        TextView text =(TextView) findViewById(R.id.gotoRegister); //get the textview id
+        text.setOnClickListener(new View.OnClickListener() { //create listener for textview
+            @Override
+            public void onClick(View v) {
+                startActivity(intent); //start the activity
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/warsa/chat/RegisterActivity.java
@@ -0,0 +1,30 @@
+package com.example.warsa.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void redirect(View view) {
+        final Intent intent = new Intent(RegisterActivity.this, LoginActivity.class); //connect this Activity to registerActivity
+
+        TextView text =(TextView) findViewById(R.id.gotoLogin); //get the textview id
+        text.setOnClickListener(new OnClickListener() { //create listener for textview
+
+            @Override
+            public void onClick(View v) {
+                startActivity(intent); //start the activity
+            }
+        });
+    }
+}
