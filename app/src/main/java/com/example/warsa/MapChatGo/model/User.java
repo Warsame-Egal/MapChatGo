99bf6d00372081a8fd304dccffe6446102d52665
warsameegal <warsameegal0@gmail.com>
2017-06-25 23:35:43 -0400

    added Materialize css to gradle build and updated manifest

--- app/src/main/java/com/example/warsa/MapChatGo/model/User.java
+++ app/src/main/java/com/example/warsa/MapChatGo/model/User.java
@@ -1,16 +1,13 @@
 package com.example.warsa.MapChatGo.model;
 
-public class Register {
+public class User {
 
     private int id;
-    private String firstName;
-    private String lastName;
-    private String password;
-    private String passwordMatch;
-    private int age;
+    private String name;
     private String email;
+    private String password;
 
-    public int getId(){
+    public int getId() {
         return id;
     }
 
@@ -18,44 +15,12 @@ public class Register {
         this.id = id;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getName() {
+        return name;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPasswordMatch() {
-        return passwordMatch;
-    }
-
-    public void setPasswordMatch(String passwordMatch) {
-        this.passwordMatch = passwordMatch;
-    }
-
-    public int getAge(){
-        return age;
-    }
-
-    public void setAge(int age){
-        this.age = age;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -64,5 +29,13 @@ public class Register {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
